fa5a87e4cb5076031b42d54c825e5a9dc335a9e8
sakeri-cyber <71511330+sakeri-cyber@users.noreply.github.com>
2021-08-11 19:19:12 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 9;
+let speed = 6;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -54,6 +54,7 @@ function gameEngine(){
     // If you have eaten the food, increment the score and regenerate the food
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play();
+        speed += 0.5;
         score += 1;
         if(score>hiscoreval){
             hiscoreval = score;
